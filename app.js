359637eb25717fdbc425380bf5f92d598e30b91f
Mikhail Dudka <deanwenchester13@gmail.com>
2023-02-01 01:50:19 +0200

    added user model and created a router for users and authentication

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const {v4: uuidv4} = require('uuid');
 const multer = require('multer');
 
 const feedRoutes = require('./routes/feed');
+const authRoutes = require('./routes/auth');
 
 const PORT = 8080;
 
@@ -47,6 +48,8 @@ app.use((req, res, next) => {
 })
 
 app.use('/feed', feedRoutes);
+
+app.use('/auth', authRoutes);
 
 app.use((error, req, res, next) => {
     console.log(error);
