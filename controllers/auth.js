d7616ec6a7eaa68f37461fe4420f1e1b1b6afb96
Mikhail Dudka <deanwenchester13@gmail.com>
2023-02-01 02:57:29 +0200

    add route for login and controller middleware for it, created a JWT token for authorization

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,9 @@
 const {validationResult} = require("express-validator");
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
+const e = require("express");
 
 exports.signup = (req, res, next) => {
     const errors = validationResult(req);
@@ -28,6 +30,50 @@ exports.signup = (req, res, next) => {
         })
         .then(result => {
             res.status(201).json({messagee: 'User created', userId: result._id})
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
+        })
+};
+
+exports.login = (req, res, next) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    let loadedUser;
+
+    User.findOne({email: email})
+        .then(user => {
+            if (!user) {
+                const error = new Error('A user with this email could not be found.');
+                error.statusCode = 401;
+                throw error;
+            }
+            loadedUser = user;
+            return bcrypt.compare(password, user.password);
+        })
+        .then(isEqual => {
+            if (!isEqual) {
+                const error = new Error('Wrong password!');
+                error.statusCode = 401;
+                throw error;
+            }
+            const token = jwt.sign(
+                {
+                    email: loadedUser.email,
+                    userId: loadedUser._id.toString()
+                },
+                'somesupersecret',
+                {expiresIn: '1h'}
+            );
+            res
+                .status(200)
+                .json({
+                    token: token,
+                    userId: loadedUser._id.toString()
+                })
         })
         .catch(err => {
             if (!err.statusCode) {
